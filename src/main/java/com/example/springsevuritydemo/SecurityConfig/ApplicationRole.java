8cc439d882391a2090eeaf94cee592cfee5fab18
victor <touchwithvictor@gmail.com>
2021-05-11 17:23:18 +0530

    hasAuthority

--- src/main/java/com/example/springsevuritydemo/SecurityConfig/ApplicationRole.java
+++ src/main/java/com/example/springsevuritydemo/SecurityConfig/ApplicationRole.java
@@ -1,14 +1,16 @@
 package com.example.springsevuritydemo.SecurityConfig;
 
 import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.example.springsevuritydemo.SecurityConfig.UserPermission.*;
 
 public enum ApplicationRole {
-    ADMIN(Sets.newHashSet()),
-    STUDENT(Sets.newHashSet(STUDENT_READ,STUDENT_WRITE,COURSE_READ,COURSE_WRITE));
+    ADMIN(Sets.newHashSet(STUDENT_READ,STUDENT_WRITE,COURSE_READ,COURSE_WRITE)),
+    STUDENT(Sets.newHashSet());
 
     private final Set<UserPermission> permissions;
 
@@ -18,5 +20,13 @@ public enum ApplicationRole {
 
     public Set<UserPermission> getPermissions() {
         return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthority(){
+        Set<SimpleGrantedAuthority> authorities = getPermissions().stream()
+                .map(permissions -> new SimpleGrantedAuthority(permissions.getPermission()))
+                .collect(Collectors.toSet());
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+this.name()));
+        return authorities;
     }
 }
--- src/main/java/com/example/springsevuritydemo/SecurityConfig/ApplicationSecurityConfig.java
+++ src/main/java/com/example/springsevuritydemo/SecurityConfig/ApplicationSecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.springsevuritydemo.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import static com.example.springsevuritydemo.SecurityConfig.ApplicationRole.*;
+import static com.example.springsevuritydemo.SecurityConfig.UserPermission.COURSE_WRITE;
 
 
 @Configuration
@@ -32,8 +34,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers("/api/admin/**").hasRole(ADMIN.name())
+               // .antMatchers("/api/admin/**").hasRole(ADMIN.name())
                 .antMatchers("/api/students").hasRole(STUDENT.name())
+                .antMatchers(HttpMethod.DELETE, "/api/admin/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.POST, "/api/admin/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/api/admin/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers("/api/admin/**").hasRole(ADMIN.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -46,13 +52,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails student= User.builder()
                 .username("student")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+                //.roles(STUDENT.name())
+                .authorities(STUDENT.getAuthority())
                 .build();
 
         UserDetails admin =User.builder()
                 .username("admin")
                 .password(passwordEncoder.encode("admin123"))
-                .roles(ADMIN.name())
+                // .roles(ADMIN.name())
+                .authorities(ADMIN.getAuthority())
                 .build();
         return new InMemoryUserDetailsManager(
                 student,
